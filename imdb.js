f52e5842cf54cf46d6e26349f214da002c6a9f9f
crocchi <ziocro@gmail.com>
2025-03-01 22:29:05 +0100

    update

--- imdb.js
+++ imdb.js
@@ -65,7 +65,7 @@ const showPage = (id)=>{
     console.log(info)
     document.getElementById(id).innerHTML += `
     Trama:${info[0].story} \n 
-    Trailer:<iframe width="100" height="100" src="${info[0].trailer}">
+    Trailer:<iframe width="120" height="120" src="${info[0].trailer}">
 </iframe> `
     });
 }
